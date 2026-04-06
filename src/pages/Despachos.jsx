65456470262db8efcab3a41399fea890f155aeed
Reynaldo Lusa (Coco) <reynaldomlusa@gmail.com>
2026-04-06 19:27:44 +0000

    File changes

--- src/pages/Despachos.jsx
+++ src/pages/Despachos.jsx
@@ -567,7 +567,7 @@ export default function Despachos() {
 
   const { data: cargas = [], isLoading: loadingCargas } = useQuery({
     queryKey: ["despachos"],
-    queryFn: () => base44.entities.Despacho.list("-fecha", 1000),
+    queryFn: () => base44.entities.Despacho.list("fecha", 1000),
     staleTime: 0,
   });
 
