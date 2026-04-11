c55e27f928b2035873b04bbca401babbe1d0d6ed
Reynaldo Lusa (Coco) <reynaldomlusa@gmail.com>
2026-04-11 19:33:48 +0000

    File changes

--- src/pages/Cosecha.jsx
+++ src/pages/Cosecha.jsx
@@ -25,18 +25,7 @@ export default function Cosecha() {
 
   const { data: rawRegistros = [], isLoading: loading, refetch } = useQuery({
     queryKey: ['cosechas'],
-    queryFn: async () => {
-      const PAGE = 5000;
-      let all = [];
-      let skip = 0;
-      while (true) {
-        const batch = await base44.entities.Cosecha.list('-fecha', PAGE, skip);
-        all = all.concat(batch);
-        if (batch.length < PAGE) break;
-        skip += PAGE;
-      }
-      return all;
-    },
+    queryFn: () => base44.entities.Cosecha.list('-fecha', 50000),
     staleTime: 0,
   });
 
